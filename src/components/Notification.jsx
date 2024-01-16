e91fff013b033176beb758519bdaf2c55ea866da
matthewkuria <mathewkwachira@gmail.com>
2024-01-16 23:19:41 +0300

    Fix the image paths problems

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 export default function Notification(props){
     return(
-        <section className="bg-gray-100 mt-5 flex items-center rounded-sm m-6">
+        <>
+        <section className="  flex items-center rounded-sm m-3 px-2 relative">
             <div className="">
-               <img src={props.image} alt="" className=" m-3 md:m-2 md:h-10 px-2 md:px-0" />
+               <img src={props.image} alt="" className="h-10 w-10  " />
             </div>
             <div className="m-5 relative ">
                 <p><span className="hover:text-blue-700">{props.name}</span> {props.action} <span className=" hover:text-blue-800 ">{props.actionDone}</span>{props.isOnline && <div className="bg-red-600 h-2 w-2 rounded-full absolute right-0 top-1/2"></div>}</p>
                 <p className="text-gray-500">{props.lastSeen}</p>
-            </div>
+            </div>           
         </section>
+        {
+            props.MessageSent&&
+             <div className="text-left ml-20 md:ml-80 md:mr-10 p-4 rounded-lg hover:bg-gray-300 border shadow-md w-3/4  md:w-1/2 ">
+               <p>{props.MessageSent}</p>
+            </div>
+           }
+        </>
     )
 }
--- src/data.jsx
+++ src/data.jsx
@@ -2,7 +2,7 @@ export default[
     {
         id:1,
         name:"Mark Webber",
-        img:"./images/avatar-mark-webber.webp",
+        img:"./avatar-mark-webber.webp",
         action:"reacted to your recent post",
         actionDone:"My first tournament today!",
         lastSeen:" 1m ago",
@@ -11,12 +11,69 @@ export default[
 
     },
     {
+        id:2,
+        name:" Angela Gray",
+        img:"./avatar-angela-gray.webp",
+        action:"followed you",
+        actionDone:"",
+        lastSeen:" 5m ago",
+        isOnline:true,
+        privateMessage:"",
 
     },
     {
+        id:3,
+        name:"  Jacob Thompson",
+        img:"./avatar-jacob-thompson.webp",
+        action:"has joined your group ",
+        actionDone:"Chess Club",
+        lastSeen:" 1 day ago",
+        isOnline:true,
+        privateMessage:"",
 
     },
     {
+        id:4,
+        name:" Rizky Hasanuddin",
+        img:"./avatar-rizky-hasanuddin.webp",
+        action:" sent you a private message",
+        actionDone:"",
+        lastSeen:" 5 days ago",
+        isOnline:false,
+        privateMessage:"Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+
+    },
+    {
+        id:5,
+        name:"  Kimberly Smith",
+        img:"./avatar-kimberly-smith.webp",
+        action:" commented on your picture ",
+        actionDone:"",
+        lastSeen:"   1 week ago",
+        isOnline:false,
+        privateMessage:"",
+
+    },
+    {
+        id:6,
+        name:"Nathan Peterson",
+        img:"./avatar-nathan-peterson.webp",
+        action:"  reacted to your recent post ",
+        actionDone:"5 end-game strategies  to increase your win rate",
+        lastSeen:"   2 weeks ago",
+        isOnline:false,
+        privateMessage:"",
+
+    },
+    {
+        id:7,
+        name:" Anna Kim ",
+        img:"./avatar-anna-kim.webp",
+        action:" left the group ",
+        actionDone:" Chess Club",
+        lastSeen:"   2 weeks ago",
+        isOnline:false,
+        privateMessage:"",
 
     },
 ]
